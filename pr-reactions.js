54a7744c758512d7d9543f21d48d0eb621ae1f87
Yauhen Shulitski <jsnjack@gmail.com>
2020-01-15 10:48:53 +0100

    :M: Change default pull request url to Review requests, verbose Approved status

--- pr-reactions.js
+++ pr-reactions.js
@@ -47,32 +47,43 @@ function start () {
         }
     }
 
-    if (settings.modify_pr_page && settings.organization) {
-        var element = document.querySelector("#js-pjax-container nav > a[aria-label='Pull Requests assigned to you']");
-        if (element) {
-            element.textContent = "All";
-            element.setAttribute("aria-label", "All created pull requests in organization");
-            var url = "/pulls?q=is:open is:pr archived:false user:" + settings.organization;
-            if (settings.hide_not_ready) {
-                url =url + " NOT " + not_ready_prs_key + " in:title";
+    if (settings.modify_pr_page) {
+        // TODO: replace with MO
+        setTimeout(function () {
+            var pr_statuses = document.querySelectorAll("#js-issues-toolbar span.opened-by + span > a");
+            for (var st of pr_statuses) {
+                st.textContent = st.getAttribute("aria-label");
             }
-            element.href= url;
-        }
+        }, 1000);
 
-        var element = document.querySelector("#js-pjax-container nav > a[title='Pull Requests mentioning you']");
-        if (element) {
-            element.textContent = "Approved";
-            element.setAttribute("aria-label", "All approved pull requests in organization");
-            element.href = "/pulls?q=is:open is:pr archived:false review:approved user:" + settings.organization;
-        }
-
-        var element = document.querySelector(".Header nav").querySelector("a[href='/pulls']");
-        if (element) {
-            var url = "/pulls?q=is:open is:pr archived:false user:" + settings.organization;
-            if (settings.hide_not_ready) {
-                url =url + " NOT " + not_ready_prs_key + " in:title";
+        if (settings.organization) {
+            var element = document.querySelector("#js-pjax-container nav > a[aria-label='Pull Requests assigned to you']");
+            if (element) {
+                element.textContent = "All";
+                // element.setAttribute("aria-label", "All created pull requests in organization");
+                var url = "/pulls?q=is:open is:pr archived:false user:" + settings.organization;
+                if (settings.hide_not_ready) {
+                    url =url + " NOT " + not_ready_prs_key + " in:title";
+                }
+                element.href= url;
             }
-            element.href= url;
+
+            var element = document.querySelector("#js-pjax-container nav > a[title='Pull Requests mentioning you']");
+            if (element) {
+                element.textContent = "Approved";
+                element.setAttribute("aria-label", "All approved pull requests in organization");
+                element.href = "/pulls?q=is:open is:pr archived:false review:approved user:" + settings.organization;
+            }
+
+            var element = document.querySelector(".Header nav").querySelector("a[href='/pulls']");
+            if (element) {
+                var url = "/pulls?q=is:open is:pr archived:false user:" + settings.organization;
+                url += " in:title review-requested:" + document.querySelector("meta[name='user-login']").getAttribute("content");
+                if (settings.hide_not_ready) {
+                    url = url + " NOT " + not_ready_prs_key + " in:title";
+                }
+                element.href= url;
+            }
         }
     }
 }
